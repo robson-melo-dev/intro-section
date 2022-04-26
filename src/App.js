f3bae3c5cf09245260e26b82f6c25eb40ce923ff
Robson Melo de Souza <sys.robson@gmail.com>
2022-04-26 16:13:14 -0300

    quase pronto, falta posicionar melhor os dropdowns no desktop e responsivot

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import hero from './images/image-hero-mobile.png';
 
-function dropdownMenu(){
+ function dropdownMenu(){
   var id = document.getElementById("dropdownClick");
   if (id.className === "topnav") {
       id.className += " responsive";
-      /* change topnav to topnav responsive */
+      /*change topnav to topnav responsive ;*/
   } else{id.className="topnav";}
 }
+
+function dropFeatures(){
+  var id = document.getElementById("features");
+  if (id.className === "features") {
+      id.className += "Drop";
+      /*change topnav to topnav responsive ;*/
+  } else{id.className="features";}
+}
+
+function dropCompanies(){
+  var id = document.getElementById("companies");
+  if (id.className === "companies") {
+      id.className += "Drop";
+      /*change topnav to topnav responsive ;*/
+  } else{id.className="companies";}
+}
+
+
+
+
 
 function App() {
   return (
@@ -18,8 +38,21 @@ function App() {
       <nav className="navbar"> {/* Top Navbar */}
       <p className="snap-logo">snap</p>
         <ul className="topnav" id="dropdownClick">          
-          <li><a href="#">Features</a></li>
-          <li><a href="#">Company</a></li>
+          <li><a href="javascript:void(0);" onClick={dropFeatures}>Features</a>
+          <ul id="features" className="features">
+            <li><a href="#">Todo List</a></li>
+            <li><a href="#">Calendar</a></li>
+            <li><a href="#">Reminders</a></li>
+            <li><a href="#">Planning</a></li>
+          </ul>
+          </li>
+          <li><a href="javascript:void(0);" onClick={dropCompanies}>Company</a>
+          <ul id="companies" className="companies">
+            <li><a href="#">History</a></li>
+            <li><a href="#">Our Team</a></li>
+            <li><a href="#">Blog</a></li>
+          </ul>          
+          </li>
           <li><a href="#">Carreers</a></li>
           <li><a href="#">About</a></li>          
           <li className="register"><a href="#">Register</a></li>
@@ -31,16 +64,17 @@ function App() {
       
 
       <main>
-        <img className="hero-img" src={hero} alt="" />
         <section className="main-section-copy">
-          <h1>Make remote work</h1>
-          <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.
-          </p>
-          <button className="learn-more-btn">Learn More</button>
+        <img className="hero-img" src={hero} alt="" />
+          <article className="copy">
+            <h1>Make remote work</h1>
+            <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.
+            </p>
+            <button className="learn-more-btn">Learn More</button> 
+            Some images from footer.
+           </article>
         </section>
-        <footer>
-          Some images from footer.
-        </footer>
+        
       </main>
 
 
